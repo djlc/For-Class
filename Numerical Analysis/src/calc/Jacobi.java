45d9987c4976c44448c234a862d4bfff325ecc81
djlc <cdn60127@gmail.com>
2016-11-07 12:17:14 +0900

    [add] Jacobi

---
+++ Numerical Analysis/src/calc/Jacobi.java
@@ -0,0 +1,106 @@
+package calc;
+
+public class Jacobi {
+	// 係数行列
+	double[][] A;
+	// 係数ベクトル
+	double[] b;
+	
+	// 近似解を求めるためのベクトル列
+	double[] x0, x1;
+
+	// 収束判定
+	public enum MODE {
+		ABS_ERR, // 絶対誤差 
+		ABS_RES, // 絶対残差
+		REL_ERR, // 相対誤差
+		REL_RES  // 相対残差
+	}
+	
+	// 収束判定で使用するノルム
+	public enum NORM {
+		ONE, TWO, INF
+	}
+	
+	// コンストラクタ
+	public Jacobi(double[][] A, double[] b) {
+		this.A = new double[A.length][A.length];
+		this.b = new double[A.length];
+		this.x0 = new double[A.length];
+		this.x1 = new double[A.length];
+		for (int i = 0; i < A.length; i++) {
+			for (int j = 0; j < A[0].length; j++) {
+				this.A[i][j] = A[i][j];
+			}
+			this.b[i] = b[i];
+			this.x0[i] = 0;
+			this.x1[i] = 0;
+		}
+	}
+
+	// Ax = bの解を求める
+	public double[] solve(double[] x0, double eps, int N, MODE mode, NORM norm) {
+		this.x0 = x0.clone();
+		for (int m = 1; m <= N; m++) {
+			for (int i = 0; i < A.length; i++) {
+				x1[i] = b[i];
+				for (int j = 0; j < A.length; j++) {
+					if (i != j) {
+						x1[i] -= A[i][j] * this.x0[j];
+					}
+				}
+				x1[i] /= A[i][i];
+			}
+			
+			if (getNorm(mode, norm) < eps) {
+				System.out.println(m);
+				break;
+			}
+			if (m == N) System.out.println("収束しない");
+			
+			this.x0 = x1.clone();
+		}
+		return x1;
+	}
+
+	private double getNorm(MODE mode, NORM norm) {
+		if (mode == MODE.ABS_ERR && norm == NORM.ONE) {
+			return Calc.vecNorm1(Calc.subVec(x0, x1));
+		}
+		if (mode == MODE.ABS_ERR && norm == NORM.TWO) {
+			return Calc.vecNorm2(Calc.subVec(x0, x1));
+		}
+		if (mode == MODE.ABS_ERR && norm == NORM.INF) {
+			return Calc.vecNormInf(Calc.subVec(x0, x1));
+		}
+		if (mode == MODE.REL_ERR && norm == NORM.ONE) {
+			return Calc.vecNorm1(Calc.subVec(x0, x1)) / Calc.vecNorm1(x1);
+		}
+		if (mode == MODE.REL_ERR && norm == NORM.TWO) {
+			return Calc.vecNorm2(Calc.subVec(x0, x1)) / Calc.vecNorm2(x1);
+		}
+		if (mode == MODE.REL_ERR && norm == NORM.INF) {
+			return Calc.vecNormInf(Calc.subVec(x0, x1)) / Calc.vecNormInf(x1);
+		}
+		if (mode == MODE.ABS_RES && norm == NORM.ONE) {
+			return Calc.vecNorm1(Calc.residual(A, x1, b));
+		}
+		if (mode == MODE.ABS_RES && norm == NORM.TWO) {
+			return Calc.vecNorm2(Calc.residual(A, x1, b));
+		}
+		if (mode == MODE.ABS_RES && norm == NORM.INF) {
+			return Calc.vecNormInf(Calc.residual(A, x1, b));
+		}
+		if (mode == MODE.ABS_RES && norm == NORM.ONE) {
+			return Calc.vecNorm1(Calc.residual(A, x1, b)) / Calc.vecNorm1(b);
+		}
+		if (mode == MODE.ABS_RES && norm == NORM.TWO) {
+			return Calc.vecNorm2(Calc.residual(A, x1, b)) / Calc.vecNorm2(b);
+		}
+		if (mode == MODE.ABS_RES && norm == NORM.INF) {
+			return Calc.vecNormInf(Calc.residual(A, x1, b)) / Calc.vecNormInf(b);
+		}
+
+		return 0;
+	}
+}
---
+++ Numerical Analysis/src/calc/JacobiExample.java
@@ -0,0 +1,24 @@
+package calc;
+
+import calc.Jacobi.MODE;
+import calc.Jacobi.NORM;
+
+public class JacobiExample {
+	public static void main(String[] args) {
+		double[][] A = {
+				{6, 1, 1, 1, 0},
+				{1, 7, 1, 1, 1},
+				{0, 1, 8, 1, 1},
+				{0, 0, 1, 9, 1},
+				{0, 0, 0, 1, 10},
+		};
+		double[] b = { 9, 11, 11, 11, 11 };
+		double[] x0 = { 0, 0, 0, 0, 0};
+		double eps = 1.0E-10;
+		int N = 100;
+		
+		Jacobi j = new Jacobi(A, b);
+		Calc.printVec(j.solve(x0, eps, N, MODE.REL_ERR, NORM.INF));
+		Calc.printVec(GaussianEliminationPivot.solve(A, b));
+	}
+}
