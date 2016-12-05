7992069b1dd7d55b69c8663cf4339a93cd0a0f41
djlc <cdn60127@gmail.com>
2016-12-05 15:26:12 +0900

    [add] 2012

---
+++ Numerical Analysis/src/exam/_2012/Problem3.java
@@ -0,0 +1,9 @@
+package exam._2012;
+
+public class Problem3 {
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ Numerical Analysis/src/exam/_2012/Problem5.java
@@ -0,0 +1,28 @@
+package exam._2012;
+
+import linear.GaussSeidel;
+import linear.Jacobi;
+import linear.MODE;
+import linear.NORM;
+
+public class Problem5 {
+
+	public static void main(String[] args) {
+		double[][] A = {
+				{3, -2, 0},
+				{-2, 3, -2},
+				{0, -2, 3},
+		};
+		double[] b = {1,1,1};
+		double[] x0 = {1, -1, 1};
+		double eps = 1.0E-7;
+		int N = 200;
+		
+		GaussSeidel g = new GaussSeidel(A, b);
+		Jacobi j = new Jacobi(A, b);
+		
+		g.solve(x0, eps, N, MODE.ABS_RES, NORM.ONE);
+		j.solve(x0, eps, N, MODE.ABS_RES, NORM.ONE);
+	}
+
+}
